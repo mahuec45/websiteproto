a910a508b31691b2ee8a846d7c0ffc821012743d
Mahuec45 <ec45@mahurangi.school.nz>
2025-10-08 12:02:49 +1300

    Remove unwanted files

--- hold_my_applejuuie.py
+++
@@ -1,8 +0,0 @@
-try:
-  test = input("what is your name? ").lower()
-  if test == "josh":
-    print("No it's not you cornball")
-  else:
-    print(f"hi {test}")
-except ValueError:
-    print("Please enter a valid name")
